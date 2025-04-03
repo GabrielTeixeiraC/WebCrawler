b9d5586c1d06f4e10d9f6fb40a2de03760d57dcf
Gabriel <gteixeiraca@gmail.com>
2025-04-03 00:59:43 -0300

    Crawler and parser base

---
+++ crawler/crawler.py
@@ -0,0 +1,48 @@
+from crawler import frontier
+from crawler import fetcher
+from crawler import parser
+from crawler import storage
+
+"""
+Crawler class for web crawling.
+This class is responsible for managing the crawling process, including
+fetching URLs, parsing content, and storing results.
+"""
+class Crawler:
+  def __init__(self, seeds: list[str], limit: int, debug: bool):
+    """
+    Initializes the Crawler class.
+    Args:
+        seeds (list[str]): List of seed URLs.
+        limit (int): Number of seeds to be used.
+        debug (bool): Enable debug mode.
+    """
+    self.seeds = seeds
+    self.limit = limit
+    self.debug = debug
+    self.frontier = frontier.Frontier(seeds[:limit], debug)
+    self.fetcher = fetcher.Fetcher()
+    self.parser = parser.Parser()
+    # self.storer = storage.Storage() 
+
+  def crawl(self):
+    """
+    Starts the crawling process.
+    This method fetches URLs from the frontier, parses the content,
+    and stores the results. It continues until the limit is reached or
+    there are no more URLs to crawl.
+    """
+    while self.frontier.has_urls() and self.limit > 0:
+      ## Get the next URL
+      next_url = self.frontier.get_next_url()
+      
+      ## Fetch the URL
+      url_content = self.fetcher.fetch(next_url)
+      ## Parse the content
+      self.parser.parse(url_content)
+      
+      ## Store the content
+      # self.storer.store(url_content)
+
+      ## Update the limit
+      self.limit -= 1
---
+++ crawler/parser.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+"""
+Parser class to parse HTML content using BeautifulSoup.
+"""
+
+class Parser:
+  def __init__(self):
+    """
+    Initializes the Parser class.
+    """
+    pass
+
+  def parse(self, html_content: str) -> list[str]:
+    """
+    Parses HTML content and extracts all links.
+    Args:
+        html_content (str): HTML content to be parsed.
+    Returns:
+        list[str]: List of extracted links.
+    """
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = soup.find_all('a')
+    for link in links:
+      print(link.get('href'))
