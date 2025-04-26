6eb927e6694a5c85944d4d056cdf80a0ee38071a
Gabriel <gteixeiraca@gmail.com>
2025-04-26 19:04:05 -0300

    Refactor logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -10,31 +10,31 @@ class Logger:
   """
   Initializes the Logger class.
   Args:
-    log_file (str): Path for the log file.
+    log_file_path (str): Path for the log file.
     flush_interval (float): Interval in seconds for flushing logs.
   """
-  def __init__(self, debug: bool = False, log_file: str = "log.jsonl", flush_interval: float = 3.0):
+  def __init__(self, debug: bool = False, log_file_path: str = "tmp/log.jsonl", flush_interval: float = 3.0):
     self.debug = debug
-    self.log_file = log_file
+    self.log_file_path =log_file_path 
     self.flush_interval = flush_interval
     self.chunk = []
     self.lock = threading.Lock()
     self.stop_event = threading.Event()
 
     if self.debug:
-      with open(file=self.log_file, mode="w", encoding="utf-8") as f:
+      with open(file=self.log_file_path, mode="w", encoding="utf-8") as f:
         pass
 
       self.worker_thread = threading.Thread(target=self._log_worker, name="LoggerThread", daemon=True)
       self.worker_thread.start()
 
-  def log(self, url: str, title: str, text: str, timestamp: int):
+  def log(self, url: str, title: str | None, text: str | None, timestamp: int):
     """
     Logs the fetched HTML page.
     Args:
       url (str): URL of the fetched page.
-      title (str): Title of the fetched page.
-      text (str): Text from the fetched page.
+      title (str | None): Title of the fetched page. None if debug is disabled.
+      text (str | None): Text from the fetched page. None if debug is disabled.
       timestamp (int): Timestamp for when the page was fetched.
     """
     if not self.debug:
@@ -65,14 +65,11 @@ class Logger:
     """
     Writes the logs to the log file in JSONL format.
     """
-    if not self.debug:
-      return
-
     with self.lock:
       if not self.chunk:
         return
 
-      with open(self.log_file, "a", encoding="utf-8") as f:
+      with open(self.log_file_path, "a", encoding="utf-8") as f:
         for entry in self.chunk:
           json.dump(entry, f, ensure_ascii=False)
           f.write("\n")
