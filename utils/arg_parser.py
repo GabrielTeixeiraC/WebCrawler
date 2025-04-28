dfe380622a32616b3b6d19aaf30cc0fdac0c0230
Gabriel <gteixeiraca@gmail.com>
2025-04-27 21:07:41 -0300

    Change limit description

--- utils/arg_parser.py
+++ utils/arg_parser.py
@@ -9,7 +9,7 @@ def parse_args():
   parser = argparse.ArgumentParser(description="Web Crawler Argument Parser")
 
   parser.add_argument("-s", "--seeds", type=str, required=True, help="Path to the seeds file")
-  parser.add_argument("-n", "--limit", type=int, required=True, help="Number of seeds to be used")
+  parser.add_argument("-n", "--limit", type=int, required=True, help="Limit for the number of pages to crawl")
   parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
 
   args = parser.parse_args()
